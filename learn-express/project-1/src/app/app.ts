eba2cc7a969ddcd539e94cac7093c3b0ea53048b
MR. Sovon <sovonsdp@gmail.com>
2024-05-14 20:13:45 -0500

    update

--- learn-express/project-1/src/app/app.ts
+++ learn-express/project-1/src/app/app.ts
@@ -35,14 +35,40 @@ courseRouter.post("/create-course", (req: Request, res: Response) => {
   });
 });
 
-app.get("/", logger, (req: Request, res: Response) => {
-  // console.log(req.query.email);
-  res.send("Hello Worldsss!");
-});
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.body);
+      res.send("Hello Worldsss!");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.send("Recevied!");
+});
+
+// Route error handling
+app.all("*", (error: any, req: Request, res: Response) => {
+  // console.log(error);
+  res.status(400).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// global error handling
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  res.status(400).json({
+    success: false,
+    message: "Failed ot get data",
+  });
 });
 
 export default app;
